114de31c0e3f49076fe9509a368dad3f83ca35f0
Alfred Meng <alfredfmeng@gmail.com>
2022-05-23 20:14:01 -0700

    Added Product and User schemas. Added auth

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,20 +1,38 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
 import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 
-// eslint-disable-next-line prettier/prettier
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+const cookieSecret = 'INeedThisToBeThirtyTwoCharactersLongSoHereIAmTypingAway';
+const frontendURL = 'http://localhost:7777/';
+const databaseURL =
+  'mongodb+srv://alfredfmeng:oow0DTl6HxlwPqKL@cluster0.s1fsj.mongodb.net/?retryWrites=true&w=majority';
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360,
-  secret: process.env.COOKIE_SECRET,
+  secret: cookieSecret,
 };
 
-export default config({
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+  },
+});
+
+export default withAuth({
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   server: {
     cors: {
-      origin: [process.env.FRONTEND_URL],
+      origin: [frontendURL],
       credentials: true,
     },
   },
@@ -22,8 +40,14 @@ export default config({
     adapter: 'mongoose',
     url: databaseURL,
   },
-  lists: createSchema({}),
+  lists: createSchema({
+    User,
+    Product,
+  }),
   ui: {
-    isAccessAllowed: () => true,
+    isAccessAllowed: ({ session }) => !!session?.data,
   },
+  session: withItemData(statelessSessions(sessionConfig), {
+    User: 'id name email',
+  }),
 });
