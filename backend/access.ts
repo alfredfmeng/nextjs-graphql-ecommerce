e5c80141668266341ec5e7748b6eccf34e1b0978
Alfred Meng <alfredfmeng@gmail.com>
2022-06-05 16:36:30 -0700

    Added rules

--- backend/access.ts
+++ backend/access.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
@@ -18,5 +20,23 @@ export const permissions = {
   ...generatedPermissions,
   isAwesome({ session }: ListAccessArgs): boolean {
     return session?.data.name.includes('wes');
+  },
+};
+
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    return { user: { id: session.itemId } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true; // They can read everything!
+    }
+    // They should only see available products (based on the status field)
+    return { status: 'AVAILABLE' };
   },
 };
