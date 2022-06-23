1b10fed8688bc661ca5364fc1aa8661cf3e8a544
Alfred Meng <alfredfmeng@gmail.com>
2022-06-23 14:37:47 -0500

    Reroutes account page to signin page if no user

--- frontend/pages/account.js
+++ frontend/pages/account.js
@@ -1,14 +1,17 @@
 import Head from 'next/head';
+import { useRouter } from 'next/dist/client/router';
 import { useUser } from '../components/User';
 
 export default function Account() {
+  const route = useRouter();
   const me = useUser();
+  if (!me?.name) route.push('/signin');
   return (
     <div>
       <Head>
         <title>My Account</title>
       </Head>
-      <h1>Hello {me.name}!</h1>
+      <h1>Hello {me?.name}!</h1>
     </div>
   );
 }
